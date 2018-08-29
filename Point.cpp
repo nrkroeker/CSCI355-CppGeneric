52480178420dec8ee70e6aed3eb2e934f1987681
Nathalie Kroeker <nathaliekroeker@gmail.com>
2018-08-29 13:12:21 -0400

    Fix syntaax

--- Point.cpp
+++ Point.cpp
@@ -1,6 +1,6 @@
 #include <type_traits>
 
-template <class T, typename U, typename std::enable_if<std::is_arithmetic<T>::value && std::is_arithmetic<U>::value>::type>>
+template <class T, typename U, typename std::enable_if<std::is_arithmetic<T>::value && std::is_arithmetic<U>::value>::type>
 class Point {
   public:
     Point(T x, U y): x_(x), y_(y) {};
