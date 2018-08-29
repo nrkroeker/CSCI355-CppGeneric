9673de44e854bc2c49da55729348f5c477c20afb
Nathalie Kroeker <nathaliekroeker@gmail.com>
2018-08-29 12:38:51 -0400

    Setup project

---
+++ Point.h
@@ -0,0 +1,9 @@
+#ifndef _POINT_H_
+#define _POINT_H_
+
+template <class T>
+class Point {
+
+};
+
+#endif
---
+++ driver.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main () {
+    std::out << "Hello world" std::endl;
+
+    return 0;
+}
